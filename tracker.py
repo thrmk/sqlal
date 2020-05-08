4531491f0281659b605ef86361ec8325f2e84caa
Manoj kumar <61312222+rmkdn@users.noreply.github.com>
2020-05-08 18:10:34 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -908,4 +908,4 @@ def display_page(pathname):
         
 
 if __name__ == '__main__':
-    app.run_server(debug=True,threaded=True, use_reloader=False)
+    app.run_server(debug=True,threaded=True, use_reloader=True)
