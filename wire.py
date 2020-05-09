c1bf1f47bdca4d46e6dcec896ee4184b6d5122e0
Manoj kumar <61312222+rmkdn@users.noreply.github.com>
2020-05-09 16:35:47 +0530

    Update wire.py

--- wire.py
+++ wire.py
@@ -975,4 +975,4 @@ def display_page(pathname):
         
 
 if __name__ == '__main__':
-    app.run_server(debug=True,threaded=True, use_reloader=False)
+    app.run_server(debug=True)#,threaded=True, use_reloader=False)
